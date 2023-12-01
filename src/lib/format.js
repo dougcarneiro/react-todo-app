203ce920c75728be6eb18b517815f9938f8c82fb
Douglas Carneiro <douglasefcarneiro@gmail.com>
2023-12-01 14:12:09 -0300

    contorno tecnico no bug de ficar sem data de criação temporariamente apos mudar o status do card

--- src/lib/format.js
+++ src/lib/format.js
@@ -1,9 +1,11 @@
 export function formatDate(value, pattern) {
-  const date = new Date(value).toISOString().split('T')[0];
-
-  if (pattern === 'ymd') {
-    return date
-  } else {
-    return date.split('-').reverse().join('/');
+  if (value) {
+    const date = new Date(value).toISOString().split('T')[0];
+  
+    if (pattern === 'ymd') {
+      return date
+    } else {
+      return date.split('-').reverse().join('/');
+    }
   }
 }
